eef53555ce3367b2b12154c0d3df2493e6f02606
NishiOwO <nishi@nishi.boats>
2025-08-06 01:46:53 +0900

    init

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+flags("MultiProcessorCompile")
+
+workspace("Crane")
+	configurations({
+		"Debug",
+		"Release"
+	})
+	platforms({
+		"Native",
+		"Win32",
+		"Win64"
+	})
+	defaultplatform("Native")
+
+filter("platforms:Win32")
+	system("windows")
+	architecture("x86")
+	gccprefix("i686-w64-mingw32-")
+
+filter("platforms:Win64")
+	system("windows")
+	architecture("x86_64")
+	gccprefix("x86_64-w64-mingw32-")
+
+filter({})
+
+project("crane")
+	flags("RelativeLinks")
+	kind("StaticLib")
+	language("C")
+	targetdir("lib")
+	objdir("obj")
+	filter("configurations:Debug")
+		defines({
+			"DEBUG"
+		})
+		symbols("On")
+	filter("configurations:Release")
+		defines({
+			"NDEBUG"
+		})
+		optimize("On")
